f7696fb454e74707909ea4637f7236b565548c6a
Zhansayess03 <91907236+Zhansayess03@users.noreply.github.com>
2021-10-31 15:24:25 +0600

    prc_1

---
+++ script.js
@@ -0,0 +1,12 @@
+const hamburger = document.querySelector(".nav__hamburger");
+const nav = document.querySelector(".nav__ul");
+
+hamburger.addEventListener("click", function () {
+  nav.classList.toggle("show-menu");
+
+  if ((hamburger.src = "images/icon-hamburger.svg")) {
+    hamburger.src = "images/icon-close.svg";
+  } else {
+    hamburger.src = "images/icon-hamburger.svg";
+  }
+});
