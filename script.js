c189614e65ee4e9d8f2f0247ee51cfe5f92486ff
Allaniyazov Sultanbek <allaniyazov.sultan@mail.ru>
2021-10-31 23:27:16 +0600

    final commit

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-const hamburger = document.querySelector(".nav__hamburger");
-const nav = document.querySelector(".nav__ul");
+function toggleMenu(){
+  var menuToggle = document.querySelector('.toggle');
+  var nav = document.querySelector('.navigation');
+  menuToggle.classList.toggle('active_toggle');
+  nav.classList.toggle("active_toggle");
+}
 
-hamburger.addEventListener("click", function () {
-  nav.classList.toggle("show-menu");
+// var hamburger = document.querySelector(".nav__hamburger");
+// const nav = document.querySelector(".nav__ul");
 
-  if ((hamburger.src = "images/icon-hamburger.svg")) {
-    hamburger.src = "images/icon-close.svg";
-  } else {
-    hamburger.src = "images/icon-hamburger.svg";
-  }
-});
+// hamburger.addEventListener("click", function () {
+//   nav.classList.toggle("show-menu");
+
+//   if ((hamburger.src = "images/icon-hamburger.svg")) {
+//     hamburger.src = "images/icon-close.svg";
+//   } else {
+//     hamburger.src = "images/icon-hamburger.svg";
+//   }
+// });
